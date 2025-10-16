305ce0349c6cfd93eda4d8e3e26399e6431a6028
Ivonne Valenzuela <229033087+IvonneValenzuela@users.noreply.github.com>
2025-10-16 17:59:12 +1300

    RefactoringV1

--- tests/Pageobjects/FRSandboxPage.ts
+++ tests/Pageobjects/FRSandboxPage.ts
@@ -1,38 +1,106 @@
+// pages/FRSandboxPage.ts
+
 import {type Locator, type Page } from '@playwright/test';
 
 export class FRSandboxPage {
-    
     private readonly url = 'https://thefreerangetester.github.io/sandbox-automation-testing/';
+    private readonly page: Page;
 
-    readonly page: Page;
+    // Buttons & messages
+
     readonly dynamicButton: Locator
     readonly delayedGhostMessage: Locator 
-    readonly textBoxUnAburridoTexto: Locator
-    readonly textDisplayedOnField : Locator
+    
+    // Text input
+    readonly textInput : Locator
+
+    // Checkbox & radio
+    readonly heladoCheckBox: Locator
+    readonly siRadioButton: Locator
+
+    // Select
+    readonly tennisDropdownOption: Locator
+
+    // Dropdown (día de la semana)
+    readonly dayDropdownButton: Locator
+    private dayOption = (day: string) => this.page.getByRole('link', { name: day });
+    readonly martesDropdown: Locator
+
+    // Popup
+    readonly popupButton: Locator
+    readonly popupMessage: Locator
+    readonly closePopup: Locator
 
     constructor(page: Page) {
         this.page = page;
+
+        //Dynamic button
         this.dynamicButton = page.getByRole('button', { name: 'Hacé click para generar un ID' });
         this.delayedGhostMessage = page.getByText('OMG, aparezco después de 3 segundos de haber hecho click en el botón 👻.');
-        this.textBoxUnAburridoTexto = page.getByRole('textbox', { name: 'Un aburrido texto' })
-        this.textDisplayedOnField = page.getByRole('textbox', {name: 'Un aburrido texto'})
+
+        //Text input
+        this.textInput = page.getByRole('textbox', {name: 'Un aburrido texto'});
+
+        // Checkbox & Radio
+        this.heladoCheckBox = page.getByLabel('Helado 🍧');
+        this.siRadioButton = page.getByLabel('si');
+
+        // Select
+        this.tennisDropdownOption = page.getByLabel('Dropdown');
+
+        // Dropdown (día de la semana)
+        this.dayDropdownButton = page.getByRole('button', { name: 'Día de la semana'});
+        this.martesDropdown = page.getByRole('link', { name: 'Martes' });
+
+        // Popup
+        this.popupButton = page.getByRole('button', { name: 'Mostrar popup' });
+        this.closePopup = page.getByRole('button', { name: 'Cerrar' });
+        this.popupMessage = page.getByText('¿Viste? ¡Apareció un Pop-up!');
     }
 
+    // Navigation
     async open() {
         await this.page.goto(this.url);
     }
     
+    //Dynamic ID button
     async clickOnDynamicButton(){
        await this.dynamicButton.click();
     }
-
-    async fillFieldInput(textTyped:string = 'Testing ♥'){
-        await this.textBoxUnAburridoTexto.fill(textTyped);
+    async fillTextInput(textTyped : string){
+        await this.textInput.fill(textTyped);
     }
 
+    //Checkbox
+    async checkHeladoCheckbox(){
+        await this.heladoCheckBox.check();
+    }
+    async uncheckHeladoCheckbox(){
+        await this.heladoCheckBox.uncheck();
+    }
     
-    
+    //Radio Buttons
+    async selectSiRadioButton(){
+        await this.siRadioButton.check();
+    }
+    async selectTennis(){
+        await this.tennisDropdownOption.selectOption('Tennis');
+    }
 
+    //Dropdown
+    async selectDay(dayName: 'Lunes' | 'Martes' | 'Miércoles' | 'Jueves' | 'Viernes' | 'Sábado' | 'Domingo') {
+        await this.dayDropdownButton.click();
+        const option = this.dayOption(dayName);
+        await option.waitFor({ state: 'visible' });
+        await option.click();
+    }
 
+    //Popup Messages
+    async openPopup(){
+        await this.popupButton.click();
+    }
+    async closePopupMessage(){
+        await this.closePopup.click();
+    }
 
 }
--- tests/Smoke/FreeRangeSandbox-smoke.spec.ts
+++ tests/Smoke/FreeRangeSandbox-smoke.spec.ts
@@ -1,122 +1,106 @@
-import { test, expect} from '@playwright/test';
+
+import { test, expect } from '@playwright/test';
 import { FRSandboxPage } from '../Pageobjects/FRSandboxPage';
 
-(async () => {
-    
-    
-    //const texToType = 'Testing ♥';
-    //const urlBase = 'https://thefreerangetester.github.io/sandbox-automation-testing/';
-    
-    test.describe('Automation Freerange Sandbox', () => {
+test.describe('FreeRange Sandbox - Smoke (POM)', () => {
+    let frSandboxPage : FRSandboxPage;   
 
-        test('Dynamic ID button reveals hidden element', async ({ page }) => {
-            const frSandbox = new FRSandboxPage (page);
+    test.beforeEach(async ({ page }) => {
+        frSandboxPage = new FRSandboxPage(page);
+        await frSandboxPage.open();
+    });
 
-            await test.step('Given I am on the Sandbox page', async () => {
-                // await page.goto(urlBase);
-                await frSandbox.open();
-            })
+    test('Dynamic ID button reveals hidden element', async ({}) => {
 
-            await test.step('When I click the dynamic ID button', async () => {
-                //await page.getByRole('button', { name: 'Hacé click para generar un ID' }).click();
-                await frSandbox.clickOnDynamicButton();
-            })
+        await frSandboxPage.clickOnDynamicButton();
+        await expect(frSandboxPage.delayedGhostMessage).toBeVisible();
+    })
 
-            await test.step('Then I should see the delayed message', async () => {
-                //     await expect(page.getByText('OMG, aparezco después de 3 segundos de haber hecho click en el botón 👻.')).toBeVisible();
-                await expect(frSandbox.delayedGhostMessage).toBeVisible();
-            })
-        })
-    
-        test('Text field input should work correctly', async ({ page }) => {
-            const frSandbox = new FRSandboxPage (page);
-            await test.step('Given I am on the Sandbox page of Free Range Testers', async () => {
-                //await page.goto(urlBase);
-                await frSandbox.open();
-            })
-            await test.step('When I type text into the "Un aburrido texto" field', async () => {
-                //await page.getByRole('textbox', { name: 'Un aburrido texto' }).fill(texToType);
-                await frSandbox.fillFieldInput();
-            }) 
-            await test.step('Then the text should be displayed in the field', async () => {
-                //await expect(page.getByRole('textbox', {name: 'Un aburrido texto'})).toHaveValue(texToType);
-                await expect(frSandbox.textDisplayedOnField).toHaveValue('Testing ♥');
-            })    
-        })
-    
-        test('Checkbox selection should work correctly', async ({ page }) => {
-            await test.step('Given I am on the Sandbox page of Free Range Testers', async () => {
-                await page.goto(urlBase);
-            })
-            // Tip Tipsudo: make use of interpolation. e.g"  `my text ${myConst}`
-            await test.step('When I check the "Helado 🍧" checkbox', async () => {
-                // TODO: move 'Helado 🍧' to a const
-                await page.getByLabel('Helado 🍧').check();
-            });
-            await test.step('Then it should be checked', async () => {
-                await expect(page.getByLabel('Helado 🍧')).toBeChecked();
-            });
-            await test.step('When I uncheck the "Helado 🍧" checkbox', async () => {
-                await page.getByLabel('Helado 🍧').uncheck();
-            });
-            await test.step('Then it should not be checked', async () => {
-                await expect(page.getByLabel('Helado 🍧')).not.toBeChecked();
-            });
-        })
-
-        test('Radio button selection should work correctly', async ({ page }) => {
-            await test.step('Given I am on the Sandbox page of Free Range Testers', async () => {
-                await page.goto(urlBase);
-            })
-            await test.step('When I select the "Sí" radio button', async () => {
-                await page.getByLabel('si').check();
-            })
-            await test.step('Then the "Sí" option should be selected ', async () => {
-                await expect(page.getByLabel('si'), 'El radio button no se seleccionó').toBeChecked(); 
-            })  
-        })
-
-        test('Dropdown selection should work correctly', async ({ page }) => {
-            await test.step('Given I am on the Sandbox page of Free Range Testers', async () => {
-                await page.goto(urlBase);
-            })
-            await test.step('When I select "Tennis" from the dropdown', async () => {
-                await page.getByLabel('Dropdown').selectOption('Tennis');
-            })
-            await test.step('Then the dropdrown should be "Tennis"', async () => {
-                await expect(page.getByLabel('Dropdown')).toHaveValue('Tennis');
-            }) 
-        })
+    test('Text input should work correctly', async ({}) => {
         
-        test('Day selection from dropdown menu should work correctly', async ({ page }) => {
-            test.fail(); // heads-up: This test is meant to be failed, but 'test.fail()' will make it pass
-            await test.step('Given I am on the Sandbox page of Free Range Testers', async () => {
-                await page.goto(urlBase); 
-            })
-            await test.step('When I open the day dropdown and select "Martes"', async () => {
-                await page.getByRole('button', { name: 'Día de la semana'}).click();
-                // TODO: make 'Martes' a const
-                await page.getByRole('link',{name: 'Martes'}).click();
-            })
-            await test.step('Then the selected option should be "Martes"', async () => {
-                await expect(page.getByRole('button', { name: 'Día de la semana' })).toContainText("Martes"); // heads-up: Here the test will fail
-            })
-        })
+        await frSandboxPage.fillTextInput('Testing ♥');
+        await expect(frSandboxPage.textInput).toHaveValue('Testing ♥');
+    })
+
+    test('Checkbox selection should work correctly', async ({}) => {
         
-        test('Popup validation should work correctly', async ({ page }) => {
-            await test.step('Given I am on the Sandbox page of Free Range Testers', async () => {
-                await page.goto(urlBase);
-            })
-            await test.step('When I click on the Popup button', async () => {
-                await page.getByRole('button', { name: 'Mostrar popup' }).click();
-            })
-            await test.step('Then should see the popup message and be able to close it', async () => {
-                await expect(page.getByText('¿Viste? ¡Apareció un Pop-up!')).toHaveText('¿Viste? ¡Apareció un Pop-up!');
-                await page.getByRole('button', { name: 'Cerrar' }).click();
-            })
+        await frSandboxPage.checkHeladoCheckbox();
+        await expect(frSandboxPage.heladoCheckBox, 'The checkbox is not selected').toBeChecked();
+        
+        await frSandboxPage.uncheckHeladoCheckbox();
+        await expect(frSandboxPage.heladoCheckBox, 'The checkbox is selected').not.toBeChecked();
+    })
+
+    test('Radio button selection should work correctly', async ({ page }) => {
+        
+        
+        const frSandbox = new FRSandboxPage(page);
+        await test.step('Given I am on the Sandbox page of Free Range Testers', async () => {
+            //await page.goto(urlBase);
+            await frSandbox.open();
         })
-    });  
-      
-})();
+        await test.step('When I select the "Sí" radio button', async () => {
+            //await page.getByLabel('si').check();
+            await frSandbox.selectSiRadioButton();
+        })
+        await test.step('Then the "Sí" option should be selected ', async () => {
+            //await expect(page.getByLabel('si'), 'El radio button no se seleccionó').toBeChecked();
+            await expect(frSandbox.siRadioButton).toBeChecked();
+        })
+    })
+
+    test('Dropdown selection should work correctly', async ({ page }) => {
+        const frSandbox = new FRSandboxPage(page);
+        await test.step('Given I am on the Sandbox page of Free Range Testers', async () => {
+            //await page.goto(urlBase);
+            await frSandbox.open();
+        })
+        await test.step('When I select "Tennis" from the dropdown', async () => {
+            //await page.getByLabel('Dropdown').selectOption('Tennis');
+            await frSandbox.selectTennis();
+        })
+        await test.step('Then the dropdrown should be "Tennis"', async () => {
+            //await expect(page.getByLabel('Dropdown')).toHaveValue('Tennis');
+            await expect(frSandbox.tennisDropdownOption).toHaveValue('Tennis');
+        })
+    })
+
+    test('Day selection from dropdown menu should work correctly', async ({ page }) => {
+        //test.fail(); // heads-up: This test is meant to be failed, but 'test.fail()' will make it pass
+        const frSandbox = new FRSandboxPage(page);
+        await test.step('Given I am on the Sandbox page of Free Range Testers', async () => {
+            //await page.goto(urlBase);
+            await frSandbox.open();
+        })
+        await test.step('When I open the day dropdown and select "Martes"', async () => {
+            //await page.getByRole('button', { name: 'Día de la semana'}).click();
+            await frSandbox.selectDay('Martes');
+        })
+        await test.step('Then the selected option should be "Martes"', async () => {
+            //await expect(page.getByRole('button', { name: 'Día de la semana' })).toContainText("Martes"); // heads-up: Here the test will fail
+            await expect(page).toHaveURL(/#\/action-2/);
+        })
+    })
+
+    test('Popup validation should work correctly', async ({ page }) => {
+        const frSandbox = new FRSandboxPage(page);
+        await test.step('Given I am on the Sandbox page of Free Range Testers', async () => {
+            //await page.goto(urlBase);
+            await frSandbox.open();
+        })
+        await test.step('When I click on the Popup button', async () => {
+            //await page.getByRole('button', { name: 'Mostrar popup' }).click();
+            await frSandbox.openPopup();
+        })
+        await test.step('Then should see the popup message and be able to close it', async () => {
+            //await expect(page.getByText('¿Viste? ¡Apareció un Pop-up!')).toHaveText('¿Viste? ¡Apareció un Pop-up!');
+            //await page.getByRole('button', { name: 'Cerrar' }).click();
+            await expect(frSandbox.popupMessage).toHaveText('¿Viste? ¡Apareció un Pop-up!');
+            await frSandbox.closePopupMessage();
+        })
+    })
+});
+
+
 
 
