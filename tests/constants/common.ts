97d464efaa0cb1acda8038dc37438b83261a7838
Ivonne Valenzuela <229033087+IvonneValenzuela@users.noreply.github.com>
2025-10-21 20:36:03 +1300

    Added new a Constant file

---
+++ tests/constants/common.ts
@@ -0,0 +1,23 @@
+import path from "path";
+
+
+export const waitThreeSeconds = 3100;
+export const holdThreeSeconds = 3100;
+
+export const pdfTestPath = path.join(__dirname, '../../pdfprueba');
+
+export enum RadioButtonOption {
+    Yes = "Yes",
+    No = "No",
+};
+
+export enum RadioButtonOption2 {
+    Foo = "Foo",
+    Bar = "Bar",
+};
+
+export enum RadioButtonOption3 {
+    Going = "Going",
+    NotGoing = "Not going",
+    Maybe = "Maybe",
+};
