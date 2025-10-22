9b26be3de1fb530ed54f5de82365638669583d8c
Ivonne Valenzuela <229033087+IvonneValenzuela@users.noreply.github.com>
2025-10-22 17:29:43 +1300

    test(pom): updated smoke tests using new POM pages

--- tests/Smoke/FreeRangeSandbox-smoke.spec.ts
+++ tests/Smoke/FreeRangeSandbox-smoke.spec.ts
@@ -31,73 +31,30 @@ test.describe('FreeRange Sandbox - Smoke (POM)', () => {
         await expect(frSandboxPage.heladoCheckBox, 'The checkbox is selected').not.toBeChecked();
     })
 
-    test('Radio button selection should work correctly', async ({ page }) => {
+    test('Radio button selection should work correctly', async ({}) => {
         
+        await frSandboxPage.selectSiRadioButton();
+        await expect(frSandboxPage.siRadioButton).toBeChecked();    
+    })
+
+    test('Dropdown selection should work correctly', async ({}) => {
         
-        const frSandbox = new FRSandboxPage(page);
-        await test.step('Given I am on the Sandbox page of Free Range Testers', async () => {
-            //await page.goto(urlBase);
-            await frSandbox.open();
-        })
-        await test.step('When I select the "Sí" radio button', async () => {
-            //await page.getByLabel('si').check();
-            await frSandbox.selectSiRadioButton();
-        })
-        await test.step('Then the "Sí" option should be selected ', async () => {
-            //await expect(page.getByLabel('si'), 'El radio button no se seleccionó').toBeChecked();
-            await expect(frSandbox.siRadioButton).toBeChecked();
-        })
+        await frSandboxPage.selectTennisOption();
+        await expect(frSandboxPage.tennisDropdownOption).toHaveValue('Tennis');
     })
 
-    test('Dropdown selection should work correctly', async ({ page }) => {
-        const frSandbox = new FRSandboxPage(page);
-        await test.step('Given I am on the Sandbox page of Free Range Testers', async () => {
-            //await page.goto(urlBase);
-            await frSandbox.open();
-        })
-        await test.step('When I select "Tennis" from the dropdown', async () => {
-            //await page.getByLabel('Dropdown').selectOption('Tennis');
-            await frSandbox.selectTennis();
-        })
-        await test.step('Then the dropdrown should be "Tennis"', async () => {
-            //await expect(page.getByLabel('Dropdown')).toHaveValue('Tennis');
-            await expect(frSandbox.tennisDropdownOption).toHaveValue('Tennis');
-        })
+    test.fail('Day selection from dropdown menu should work correctly', async ({}) => {
+       //const currentUrl = await frSandboxPage.getCurrentUrl();
+        await frSandboxPage.selectDay('Martes');
+        await expect(frSandboxPage.martesDropdown).toContainText("Martes"); // heads-up: Here the test will fail
+        //await expect(currentUrl).toContain('#/action-2');  
     })
 
-    test('Day selection from dropdown menu should work correctly', async ({ page }) => {
-        //test.fail(); // heads-up: This test is meant to be failed, but 'test.fail()' will make it pass
-        const frSandbox = new FRSandboxPage(page);
-        await test.step('Given I am on the Sandbox page of Free Range Testers', async () => {
-            //await page.goto(urlBase);
-            await frSandbox.open();
-        })
-        await test.step('When I open the day dropdown and select "Martes"', async () => {
-            //await page.getByRole('button', { name: 'Día de la semana'}).click();
-            await frSandbox.selectDay('Martes');
-        })
-        await test.step('Then the selected option should be "Martes"', async () => {
-            //await expect(page.getByRole('button', { name: 'Día de la semana' })).toContainText("Martes"); // heads-up: Here the test will fail
-            await expect(page).toHaveURL(/#\/action-2/);
-        })
-    })
-
-    test('Popup validation should work correctly', async ({ page }) => {
-        const frSandbox = new FRSandboxPage(page);
-        await test.step('Given I am on the Sandbox page of Free Range Testers', async () => {
-            //await page.goto(urlBase);
-            await frSandbox.open();
-        })
-        await test.step('When I click on the Popup button', async () => {
-            //await page.getByRole('button', { name: 'Mostrar popup' }).click();
-            await frSandbox.openPopup();
-        })
-        await test.step('Then should see the popup message and be able to close it', async () => {
-            //await expect(page.getByText('¿Viste? ¡Apareció un Pop-up!')).toHaveText('¿Viste? ¡Apareció un Pop-up!');
-            //await page.getByRole('button', { name: 'Cerrar' }).click();
-            await expect(frSandbox.popupMessage).toHaveText('¿Viste? ¡Apareció un Pop-up!');
-            await frSandbox.closePopupMessage();
-        })
+    test('Popup validation should work correctly', async ({}) => {
+        
+        await frSandboxPage.openPopupMessage();
+        await expect(frSandboxPage.popupMessage).toHaveText('¿Viste? ¡Apareció un Pop-up!');
+        await frSandboxPage.closePopupMessage();   
     })
 });
 
--- tests/Smoke/LetCodeSandbox-smoke.spec.ts
+++ tests/Smoke/LetCodeSandbox-smoke.spec.ts
@@ -1,211 +1,126 @@
-import { test, expect} from '@playwright/test';
-import path from 'path';
 
-(async () => {
+import { test, expect } from '@playwright/test';
+import { LCSandboxPage, RadioLabels } from '../Pageobjects/LCSandboxPage';
+import { RadioButtonOption,RadioButtonOption3, RadioButtonOption2 } from '../constants/common';
+
+
+test.describe('Smoke test on Letcode Sandbox page', () => {
+    let letCodeSandboxPage : LCSandboxPage;
     
-    const letCodeUrl = 'https://letcode.in/test';
-    const testingProjectText = 'Testing Project';
-    const pdfTestPath = path.join(__dirname, '../../pdfprueba');
-    const threeSeconds = 3100;
+    test.beforeEach(async ({ page }) => {
+        letCodeSandboxPage = new LCSandboxPage(page);
+        await letCodeSandboxPage.open();
+    });
 
-    
-    test.describe('Smoke test on Letcode Sandbox page', () => {
+    test('Dynamic button and hidden element should work correctly', async ({}) => {
 
-        test('Dynamic button and hidden element should work correctly', async ({ page }) => {
-
-            await test.step('Given I am on the LetCode Button page', async () => {
-                await page.goto(letCodeUrl);
-                await page.getByRole('link', { name:  'Click' }).click();
-            })
-            await test.step('When I click and hold the "Botton Hold!" dynamic button', async () => {
-                await page.getByRole('button', { name: 'Button Hold!' }).click({ delay: threeSeconds });
-            }) 
-            await test.step('Then I should see the delayed message', async () => {
-                await expect(page.getByText('Button has been long pressed')).toBeVisible();
-            })
-        })
-    
-        test ('Text field should accept and display input correctly', async ({ page }) => {
-            const fullNameTextBox = page.getByRole('textbox', { name: 'Enter first & last name' });
-            
-            await test.step('Given I am on the LetCode Input page', async () => {
-                await page.goto(letCodeUrl);
-                await page.getByRole('link', { name:  'Edit' }).click();
-            })
-            await test.step('When I type text into the "Enter your full Name" field', async () => {
-                await fullNameTextBox.fill(testingProjectText);
-            })
-            await test.step('Then the text should be displayed in the "Enter your full Name" field', async () => {
-                await expect(fullNameTextBox).toBeEditable();
-                await expect(fullNameTextBox).toHaveValue(testingProjectText); 
-            })
-        })
-    
-        test('Checkbox selection should work correctly', async ({ page }) => {
-            const remembermeCheckBox = page.getByRole('checkbox', { name: 'Remember me' });
-
-            await test.step('Given I am on the LetCode Checkboxes page', async () => {
-                await page.goto(letCodeUrl);
-                await page.getByRole('link', { name:  'Toggle' }).click();
-            })
-            await test.step('When I check the "Remember me" checkbox', async () => {
-                await remembermeCheckBox.check();
-            })
-            await test.step('Then it should be checked', async () => {
-                await expect(remembermeCheckBox).toBeChecked();
-            })
-             await test.step('When I uncheck the "Remember me" checkbox', async () => {
-                await remembermeCheckBox.uncheck();
-            })
-            await test.step('Then it should be unchecked', async () => {
-                await expect(remembermeCheckBox).not.toBeChecked();
-            })
-        })
-
-        test('Radio button selection should work correctly', async ({ page }) => {
-            const optionRB1 = page.getByText('Yes').first();
-            const optionRB2 = page.getByText('Yes').nth(1);
-            const optionRB3 = page.getByText('No').nth(2);
-            const optionRB4 = page.getByText('Foo');
-            const optionRB5 = page.getByText('Not going');
-
-            await test.step('Given I am on the LetCode Radio Button page', async () => {
-                await page.goto(letCodeUrl);
-                await page.getByRole('link', { name:  'Toggle' }).click();
-            })
-            await test.step('When I select "Yes" on the first and second radio buttons, and "No" on the third radio button', async () => {
-                await optionRB1.check();
-                await optionRB2.check();
-                await optionRB3.check();
-            })
-            await test.step('Then the first and second "Yes" and the third "No" radio buttons should be selected', async () => {
-                await expect(optionRB1).toBeChecked();
-                await expect(optionRB2).toBeChecked();
-                await expect(optionRB3).toBeChecked();
-            })
-            await test.step('When I select the "Foo" on the fourth and "Not going" on the fifth radio button', async () => {
-                await optionRB4.check();
-                await optionRB5.check();
-            })
-            await test.step('Then the "Foo" on the fourth and "Not going" on the fifth radio button should be selected', async () => {
-                await expect(optionRB4).toBeChecked();
-                await expect(optionRB5).toBeChecked();
-            })
-        })
-
-        test('Dropdown selection should work correctly', async ({ page }) => {
-            const mangoValue='1';
-
-            await test.step('Given I am on the LetCode Dropdown page', async () => {
-                await page.goto(letCodeUrl);
-                await page.getByRole('link', { name:  'Drop-Down' }).click();
-            })
-            await test.step('When I select "Mango" from the dropdown', async () => {
-                await page.locator('#fruits').selectOption(mangoValue);// TODO: define '1' as a constant    
-            })
-            await test.step('Then "Mango" should be selected on the dropdown and a message should appear', async () => {
-                //await expect(page.locator('#fruits')).toContainText("Mango"); Another way to verify the assertion
-                await expect(page.locator('#fruits')).toHaveValue(mangoValue);
-                await expect(page.getByText('You have selected Mango')).toBeVisible();
-            })
-        })
-
-        
-        test('Programming language selection from dropdown menu should work correctly', async ({ page }) => {
-
-            await test.step('Given I am on the LetCode Dropdown page', async () => {
-                await page.goto(letCodeUrl);
-                await page.getByRole('link', { name:  'Drop-Down' }).click();
-            })
-            await test.step('When I select "Python" from the dropdown', async () => {
-                await page.locator('#lang').selectOption('Python');
-            })
-            await test.step('Then the dropdown should display "Python"', async () => {
-                await expect(page.locator('#lang')).toContainText('Python'); 
-                await expect(page.locator('#lang')).toHaveValue('py');
-                
-                /** (copilot suggestion)
-                 Best practice:
-                    - Use toHaveValue for reliable, programmatic checks.
-                    - Use toContainText if you need to verify the displayed text for the user.
-                    For most automation scenarios, toHaveValue is preferred for dropdowns. You can use both together for thorough validation.                 
-                 */
-            }) 
-        })
-        
-        test('Select the last programming language and print all the options should work correctly', async ({ page }) => {
-
-            await test.step('Given I am on the LetCode Dropdown page', async () => {
-                await page.goto(letCodeUrl);
-                await page.getByRole('link', { name:  'Drop-Down' }).click();
-            })
-            await test.step('When I select the last programming language and print all the language options', async () => {
-
-                //finding the last option without haveing to explicitly put 'C#
-                //const options = await page.locator('#lang option').all();
-                //const lastOptionValue = await options[options.length - 1].getAttribute('value');
-                //await page.locator('#lang').selectOption(lastOptionValue!);
-        
-                //await page.locator('#lang').selectOption('C#');// 50ms - Another way 
-                //await page.locator('#lang').selectOption('sharp');// 40ms - Another way
-                await page.locator('#lang').selectOption({ label: 'C#' }); // 24ms (more efficient)
-            })
-            await test.step('Then the selected option should be "C#"', async () => {
-                await expect(page.locator('#lang')).toHaveValue('sharp')
-                console.log("All options:", (await page.locator('#lang').allInnerTexts()).join());
-            })
-            
-        })
-        
-        test('Select "India" option using value & print the selected value should work correctly', async ({ page }) => {
-
-            await test.step('Given I am on the LetCode Dropdown page', async () => {
-                await page.goto(letCodeUrl);
-                await page.getByRole('link', { name:  'Drop-Down' }).click();
-            })
-            await test.step('When I select "India" from the country dropdown and print it', async () => {
-                await page.locator('#country').selectOption('India');
-            })
-            await test.step('Then the selected option should be "India"', async () => {
-                await expect(page.locator('#country')).toHaveValue('India')
-                console.log("Select value:", await page.locator('#country').selectOption('India'));
-            })
-        })
-    
-        test('Upload file should work correctly', async ({ page }) => {
-
-            await test.step('Given I am on the LetCode File Management page', async () => {
-                await page.goto(letCodeUrl);
-                await page.getByRole('link', { name:  'File management' }).click();
-            })
-            await test.step('When I upload a file', async () => {
-                await page.getByLabel(' Choose a file… ').setInputFiles(pdfTestPath + '/TestingDoc.pdf'); 
-            })
-            await test.step('Then a file should be uploaded successfully', async () => {
-                await expect(page.getByText('Selected File')).toBeVisible();
-            })           
-        })
-    
-        test.fail('Drag and Drop functionality should work correctly', async ({ page }) => {
-            //Test is not working
-            const draggable = page.locator('#draggable'); 
-            const dropTarget = page.locator('#droppable');
-
-            await test.step('Given I am on the LetCode Drop page', async () => {
-                await page.goto(letCodeUrl);
-                await page.getByRole('link', { name:  'AUI - 2' }).click();
-            })
-            await test.step('When I drag the Green element to the purple one', async () => {
-                await draggable.dragTo(dropTarget);
-            })
-            await test.step('Then the green element should be dragged successfully', async () => {
-                await expect(dropTarget).toContainText('Drag me to my target');
-            })   
-
-        })
-        
+        await letCodeSandboxPage.goToButtonSection();
+        await letCodeSandboxPage.clickOnDynamicButtonHold();
+        await expect(letCodeSandboxPage.delayedMessageVisible).toBeVisible();
     })
 
-})();
+    test('Text field accepts user input and displays it correctly', async ({}) => {
+        
+        await letCodeSandboxPage.goToInputSection();
+        await letCodeSandboxPage.typeTextIntofullNameTextBox('Margarita');
+        await expect(letCodeSandboxPage.fullNameTextBox).toBeEditable();
+        await expect(letCodeSandboxPage.fullNameTextBox).toHaveValue('Margarita');
+    })
+
+    test('Checkbox selection should work correctly', async ({}) => {
+        
+        await letCodeSandboxPage.goToRadioSection();
+        await letCodeSandboxPage.checkRememberMeCheckbox();
+        await expect(letCodeSandboxPage.rememberMeCheckbox, 'The checkbox is not selected').toBeChecked();
+        
+        await letCodeSandboxPage.uncheckRememberMeCheckbox();
+        await expect(letCodeSandboxPage.rememberMeCheckbox, 'The checkbox is selected').not.toBeChecked();
+    })
+
+    test('1.1 Radio button selection should work correctly', async ({}) => {
+        
+        await letCodeSandboxPage.goToRadioSection();
+        await letCodeSandboxPage.selectRadioButton('Select any one', 'Yes');
+        expect(await letCodeSandboxPage.radioButtonSelected('Select any one', 'Yes')).toBe(true);
+        
+        await letCodeSandboxPage.selectRadioButton('Cofirm you can select only one radio button', 'No');
+        expect(await letCodeSandboxPage.radioButtonSelected('Cofirm you can select only one radio button', 'No')).toBe(true);
+
+        await letCodeSandboxPage.selectRadioButton('Find the bug', 'No');
+        expect(await letCodeSandboxPage.radioButtonSelected('Find the bug', 'No')).toBe(true);
+
+        await letCodeSandboxPage.selectRadioButton('Find which one is selected', 'Bar');
+        expect(await letCodeSandboxPage.radioButtonSelected('Find which one is selected', 'Bar')).toBe(true);
+
+        await letCodeSandboxPage.selectRadioButton('Confirm last field is disabled', 'Going');
+        expect(await letCodeSandboxPage.radioButtonSelected('Confirm last field is disabled', 'Going')).toBe(true);
+
+    })
+
+    test('1.2 Radio button selection should work correctly', async ({}) => {
+        
+        await letCodeSandboxPage.goToRadioSection();
+        await letCodeSandboxPage.selectRadioYesOrNoByText(RadioLabels.SelectAnyOne, RadioButtonOption.Yes);  
+        expect(letCodeSandboxPage.selectRadioYesOrNoByText).toBeTruthy();//Como se valida que se ahya elegido el Yes
+
+        await letCodeSandboxPage.selectRadioYesOrNoByText(RadioLabels.CofirmYouCanSelectOnlyOneRadioButton, RadioButtonOption.No);
+        expect(letCodeSandboxPage.selectRadioYesOrNoByText).toBeTruthy();
+
+        await letCodeSandboxPage.selectRadioYesOrNoByText(RadioLabels.FindTheBug, RadioButtonOption.Yes);
+        expect(letCodeSandboxPage.selectRadioYesOrNoByText).toBeTruthy();
+
+        await letCodeSandboxPage.selectRadioFooBarByText(RadioButtonOption2.Foo);
+        expect(letCodeSandboxPage.selectRadioFooBarByText).toBeTruthy();
+
+        await letCodeSandboxPage.selectRadioGoingNotGoingMaybeByText(RadioButtonOption3.NotGoing);
+        expect (letCodeSandboxPage.selectRadioGoingNotGoingMaybeByText).toBeTruthy();
+
+    })
+    
+
+    test('1.1 Fuit Dropdown selection should work correctly', async ({}) => {
+        
+        await letCodeSandboxPage.goToDropdownSection();
+        await letCodeSandboxPage.selectFruitByLabel('Banana');
+        await expect(letCodeSandboxPage.selectedMessageDisplayed).toContainText('Banana');
+    })
+
+    test('1.2 Fuit Dropdown selection should work correctly', async ({}) => {
+        
+        await letCodeSandboxPage.goToDropdownSection();
+        await letCodeSandboxPage.selectFruitByLabelAndValue('Mango','1');
+        const selected = await letCodeSandboxPage.getSelectedFruit();
+        expect (selected.value).toBe('1');
+        expect (selected.label).toBe('Mango');
+    })
+
+
+    test('Programming language dropdown selection should work correctly', async ({}) => {
+
+        await letCodeSandboxPage.goToDropdownSection();
+        await letCodeSandboxPage.selectProgramingLanguageByValue('py');
+        await expect(letCodeSandboxPage.selectedMessageDisplayed).toHaveText('You have selected Python')
+        //await expect(letCodeSandboxPage.selectedMessageDisplayed).toContainText('Python')  
+    })
+
+    test('Select the last programming language and print all the options should work correctly', async ({ page }) => {
+
+        await letCodeSandboxPage.goToDropdownSection();
+        await letCodeSandboxPage.selectProgramingLanguageByValue('C#');
+        await expect(letCodeSandboxPage.selectedMessageDisplayed).toHaveText('You have selected C#')
+        console.log();//no se como hacerlo
+    })
+
+
+    test('Upload file should work correctly', async ({}) => {
+
+        await letCodeSandboxPage.goToFileSection();
+        await letCodeSandboxPage.uploadFile();
+        await expect(letCodeSandboxPage.selectedFileMessage).toContainText('Selected File');
+    })
+
+});
+
+
 
 
